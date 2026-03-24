1a079a51f2d2c33cb5412b74116e975632bc79a5
BestBor <185581264+BestBor@users.noreply.github.com>
2026-03-23 20:09:12 -0500

    :sparkles: feat: implement player movement

--- constants.py
+++ constants.py
@@ -1,3 +1,15 @@
+# Screen
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
+
+# Player drawing
+PLAYER_RADIUS = 20
+LINE_WIDTH = 2
+
+# Player move
+PLAYER_TURN_SPEED = 300
+PLAYER_SPEED = 200
+
+
+
 
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import pygame
+
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
+from player import Player
 
 def main():
     print(f"Starting Asteroids with pygame version: {pygame.version.ver}")
@@ -9,6 +11,13 @@ def main():
 
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    # delta time
+    dt = 0
+
+    # player status
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
 
     while True:
         log_state()
@@ -16,9 +25,14 @@ def main():
             if event.type == pygame.QUIT:
                 return
             pass
+
         
         screen.fill("black")
+        player.update(dt)
+        player.draw(screen)
         pygame.display.flip()
+        dt = clock.tick(60)/1000
+
 
 
 if __name__ == "__main__":
