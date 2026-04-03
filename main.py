5801557fe72c2589533f254915a6383585272168
BestBor <185581264+BestBor@users.noreply.github.com>
2026-04-03 13:39:49 -0500

    :sparkles: feat: add collision control with asteroids

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import pygame
+import sys
 
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
-from logger import log_state
+from logger import log_state, log_event
 from player import Player
 from asteroid import Asteroid
 from asteroidfield import AsteroidField
@@ -46,6 +47,12 @@ def main():
 
         for element in drawable:
             element.draw(screen)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                log_event("player_hit")
+                print("Game over!")
+                sys.exit()
 
             
         pygame.display.flip()
